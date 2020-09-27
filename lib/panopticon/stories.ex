54308065e56740ed0a11b263acd0e703f2094ac7
Jaime Gomes <jaime.gomes@protonmail.com>
2020-09-27 10:09:06 +0200

    add stats

--- lib/panopticon/stories.ex
+++ lib/panopticon/stories.ex
@@ -7,6 +7,10 @@ defmodule Panopticon.Stories do
     |> Enum.concat(fetch_github_issues())
   end
 
+  def is_assigned?(stories) do
+    !is_nil(stories.assignee)
+  end
+
   defp fetch_github_issues() do
     with { :ok, body } <- File.read("./assets/json/github.json"),
          { :ok, json } <- Poison.decode(body),
@@ -62,100 +66,5 @@ defmodule Panopticon.Stories do
   defp set_github_type(_), do: "chore"
 
 
-  def list_stores do
-    [
-      %{
-        assignee: %{
-          id: 2063848,
-          login: "thardeck",
-          type: "User",
-        },
-        assignees: [
-          %{
-            id: 2063848,
-            login: "thardeck",
-            type: "User",
-          }
-        ],
-        body: "The SUSE .net buildpack was not updated since April because there are issues in our .net update pipeline.\r\nhttps://github.com/SUSE/cf-dotnet-core-buildpack/releases\r\n\r\n## Acceptance criteria\r\n* The latest .net upstream buildpack is also available for the SUSE stack",
-        comments: 0,
-        id: 691956143,
-        labels: [
-          %{
-            color: "e65c00",
-            default: false,
-            description: "",
-            id: 1709895209,
-            name: "Priority: High",
-            node_id: "MDU6TGFiZWwxNzA5ODk1MjA5",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/Priority:%20High"
-          },
-          %{
-            color: "e6b800",
-            default: false,
-            description: "",
-            id: 1709902801,
-            name: "Type: Maintenance",
-            node_id: "MDU6TGFiZWwxNzA5OTAyODAx",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/Type:%20Maintenance"
-          },
-          %{
-            color: "001500",
-            default: false,
-            description: "",
-            id: 2011893458,
-            name: "suse-cap",
-            node_id: "MDU6TGFiZWwyMDExODkzNDU4",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/suse-cap"
-          }
-        ],
-        number: 1284,
-        state: "open",
-        title: "Update SUSE .net buildpack",
-        updated_at: "2020-09-11T11:23:36Z",
-        url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/issues/1284",
-        html_url: "https://github.com/cloudfoundry-incubator/kubecf/issues/1284",
-      },
-      %{
-        body: "## Acceptance Criteria\r\n\r\n- update the buildpacks (SLE15) bump pipeline to write to the new files\r\n- update the stemcell bump pipeline to write to the new files\r\n- pause the pipelines that bump stuff for SCF\r\n- use https://www.conventionalcommits.org/en/v1.0.0/ structure for prs\r\n\r\n**Files**\r\nhttps://github.com/cloudfoundry-incubator/kubecf/blob/master/chart/config/sle15.yaml\r\n\r\nFollow-up of #1086, needed for #1164",
-        id: 687381766,
-        labels: [
-          %{
-            color: "e60000",
-            default: false,
-            description: "",
-            id: 1709894520,
-            name: "Priority: Critical",
-            node_id: "MDU6TGFiZWwxNzA5ODk0NTIw",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/Priority:%20Critical"
-          },
-          %{
-            color: "f0dc59",
-            default: false,
-            description: "",
-            id: 1934824810,
-            name: "Size: 5",
-            node_id: "MDU6TGFiZWwxOTM0ODI0ODEw",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/Size:%205"
-          },
-          %{
-            color: "001500",
-            default: false,
-            description: "",
-            id: 2011893458,
-            name: "suse-cap",
-            node_id: "MDU6TGFiZWwyMDExODkzNDU4",
-            url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/labels/suse-cap"
-          }
-        ],
-        number: 1262,
-        repository_url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf",
-        state: "open",
-        title: "Update pipelines that bump stemcells and buildpacks to update the new locations",
-        updated_at: "2020-09-11T11:12:52Z",
-        url: "https://api.github.com/repos/cloudfoundry-incubator/kubecf/issues/1262",
-        html_url: "https://github.com/cloudfoundry-incubator/kubecf/issues/1284",
-      }
-    ]
-  end
+
 end
--- lib/panopticon_web/live/stories_live.ex
+++ lib/panopticon_web/live/stories_live.ex
@@ -7,97 +7,16 @@ defmodule PanopticonWeb.StoriesLive do
     socket = assign(
       socket,
       stories: Stories.fetch_all_stories(),
-      type: ""
+      total: 0,
+      progress: 0,
+      waiting: 0
     )
 
     { :ok, socket }
   end
 
-  def render(assigns) do
-    ~L"""
-    <div id="stories">
-      <div class="mt-1 max-w-4xl mx-auto flex p-6 bg-gray-300 rounded-lg">
-        <div id="filter">
-          <form phx-change="filter">
-            <div class="inline-block relative w-full">
-              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="sources">
-                Source
-              </label>
-              <select class="block appearance-none w-full bg-white-300 border text-gray-700 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="sources">
-                <%= options_for_select(source_options(), "sources") %>
-              </select>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div class="mt-1 max-w-4xl mx-auto flex p-6 bg-gray-300 rounded-lg">
-        <ul>
-          <%= for story <- @stories do %>
-            <li class="mt-1 p-3 bg-gray-100 rounded-lg shadow-xl">
-              <!-- First Line -->
-              <div class="flex items-center justify-between">
-                <!-- group -->
-                <div class="mt-0 flex items-center">
-                  <%= select_source_logo(story) %>
-                  <a class="ml-4 font-medium text-base text-indigo-400 hover:text-indigo-800 "href="<%= story.url%>" target="_blank">
-                    <%= story.title %>
-                  </a>
-                </div>
-              </div>
-              <!-- Second Line -->
-              <div class="flex mt-4 items-center justify-between">
-                <!-- group -->
-                <div class="mt-0 flex items-center">
-                  <!-- state button -->
-                  <span class="px-3 py-1 rounded text-xs text-white bg-gray-700">
-                    <%= if is_assignee?(story) do %>
-                      progress
-                    <%= else %>
-                      waiting
-                    <% end %>
-                  </span>
-                  <!-- issue type -->
-                  <div class="ml-3 px-3 py-1 items-center">
-                    <img class="mr-1 h-4 w-4 inline" src="/images/ban.svg">
-                    <span class="text-xs text-gray-500">
-                      <%= story.type %>
-                    </span>
-                  </div>
-                  <!-- issue type -->
-                    <div class="ml-3 px-3 py-1 items-center">
-                    <img class="mr-1 h-4 w-4 inline" src="/images/arrow-circle-up.svg">
-                    <span class="text-xs text-gray-500">
-                      high
-                    </span>
-                  </div>
-                  <!-- assignee name -->
-                  <div class="ml-3 px-3 py-1 items-center">
-                    <img class="mr-1 h-4 w-4 inline" src="/images/users.svg">
-                    <span class="text-xs text-gray-500">
-                      <%= story.assignee %>
-                    </span>
-                  </div>
-                </div>
-                <!-- expand button -->
-                <button class="flex items-center py-1 px-3 ml-2 bg-transparent rounded outline-none">
-                  <img class="mr-1 h-4 w-4 inline" src="/images/chevron-down.svg">
-                </button>
-              </div>
-            </li>
-          <% end %>
-        </ul>
-      </div>
-    </div>
-    """
-  end
-
-  defp is_assignee?(%{assignee: _}), do: true
-  defp is_assignee?(_), do: false
-
   defp select_source_logo(story) do
     assigns = {}
-    IO.inspect story.source
     ~L"""
       <img class="mr-1 h-6 w-6 inline"
         <%= cond do
@@ -111,16 +30,22 @@ defmodule PanopticonWeb.StoriesLive do
     """
   end
 
-  defp select_source_logo() do
-
+  defp source_logo(story) do
+    cond do
+      story.source == "pivotal" ->
+        "/images/pivotal.png"
+      story.source == "github" ->
+        "/images/github.png"
+      story.source == "jira" ->
+        "/images/github.png"
+    end
   end
 
-  defp source_options do
-    [
-      "*": "all",
-      github: "github",
-      pivotal: "pivotal",
-      jira: "jira"
-    ]
+  defp waiting_bg_colour(story) do
+    if Stories.is_assigned?(story), do: "text-white bg-gray-700", else: "text-white bg-orange-700"
+  end
+
+  defp statistics(stories) do
+    %{total: 10, progress: 5, waiting: 1}
   end
 end
